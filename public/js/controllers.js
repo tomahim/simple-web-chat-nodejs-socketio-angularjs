572051f46f6a7a8fa349f63a68eaf7818cca273e
Thomas Himblot <thimblot@gmail.com>
2013-10-04 16:52:37 +0200

    adding features like managing user list

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,18 +5,35 @@
 function TchatCtrl($scope, socket) {
 
 	$scope.messages = [];
+	$scope.users = [];	
+	$scope.local_user = {};
 
 	socket.on('send:message', function (data) {
-        $scope.messages.push(data.message);
+        $scope.messages.push(messageToDisplay(data));
+    });
+
+	socket.on('user:join', function (user) {
+		$scope.users.push(user);
+		$scope.local_user = user;
     });
 
 	$scope.sendMessage = function() {		
-        socket.emit('send:message', {message: $scope.message});
+		var data = {message: $scope.message, user : $scope.local_user};
+        socket.emit('send:message', data);
         
         // add the message to our model locally
-	    $scope.messages.push($scope.message);
+	    $scope.messages.push(messageToDisplay(data));
 
 	    // clear message box
 	    $scope.message = '';
+	}
+
+	function messageToDisplay(data) {
+		var message = "";
+		if(data.user) {
+			message = data.user.name + " : ";
+		}
+		message += data.message;		
+		return message;
 	}
 }
--- socket.js
+++ socket.js
@@ -1,9 +1,44 @@
-// export function for listening to the socket
-module.exports = function (socket) {
-  
-      socket.emit('send:message', {message  : 'Bienvenue sur le chat' });
+// export function for listening to the socket on the connection
+var users = [];
 
-      socket.on('send:message', function (data) {
-          socket.broadcast.emit('send:message', data);
-      });
+module.exports = function (socket) {	
+
+    socket.on('disconnect', function () {
+		socket.broadcast.emit('send:message', {message : new_user.name + ' a quitté la discussion'});
+		users.pop();
+    });
+
+    var new_user = {id : (users.length+1), name : generateGuestName()};
+    users.push(new_user);
+    console.log(users);
+
+    socket.emit('send:message', {message  : '*** Bienvenue sur le chat ' + new_user.name + ' ***' });
+
+	socket.broadcast.emit('send:message', {message : new_user.name + ' a rejoint la discussion'});
+
+    socket.emit('user:join', new_user);
+
+	socket.on('send:message', function (data) {
+    	socket.broadcast.emit('send:message', data);
+    });
+
+    socket.on('user:join', function (user) {
+    	socket.broadcast.emit('user:join', user);
+    });
 };
+
+
+//Try to generate unique name
+function generateGuestName() {
+	var num = (users.length+1);
+	var name = 'Guest_' + num;
+	for (var i = 0; i <= users.length-1; i++) {
+		if(users[i].name != name) {
+			break;
+		} else {
+			num++;
+			name = 'Guest_' + (users.length+1);
+		}
+	};
+	return name;
+}
