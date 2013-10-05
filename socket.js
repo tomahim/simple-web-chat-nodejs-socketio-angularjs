24d79b320fc17424e7d20726dd61e89041f347b0
Thomas Himblot <thimblot@gmail.com>
2013-10-06 01:59:29 +0200

    try not to have duplicate username

--- socket.js
+++ socket.js
@@ -5,13 +5,14 @@ module.exports = function (socket) {
 
     socket.on('disconnect', function () {
 		socket.broadcast.emit('send:message', {message : new_user.name + ' a quitté la discussion'});
-		users.splice(new_user.id-1, new_user.id-1);
+		users.splice(new_user.id-1, 1);
     });
 
     var new_user = {id : (users.length+1), name : generateGuestName()};
     users.push(new_user);
     console.log("**********************");
     console.log(users);
+    console.log(users[0].name);
     console.log("**********************");
 
     socket.emit('send:message', {message  : '*** Bienvenue sur le chat ' + new_user.name + ' ***' });
@@ -33,5 +34,11 @@ module.exports = function (socket) {
 
 //Try to generate unique name
 function generateGuestName() {
-	return 'Guest_' + (users.length+1);
+	var num = 1;
+	var name = 'Guest_' + num;
+	while(users[(num-1)] != null && users[(num-1)].name == name) {
+		num++;
+		name = 'Guest_' + num;
+	}
+	return name;
 }
